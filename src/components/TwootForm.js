4efebb2c509812970406c58fd18aba91d08c77f7
jmigsan <>
2022-04-28 14:49:21 +0100

    no wait, heres logos. also twootform.js.

---
+++ src/components/TwootForm.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function TwootForm() {
+  return (
+    <div>
+      <form action="/">
+        <label for="fname">name: </label> <br />
+        <input type="text" id="fname" name="fname" value="John Twutter"/>
+        <br />
+        <br />
+        <label for="msg">twoot: </label>
+        <br />
+        <textarea type="text" id="msg" name="msg" value=""/>
+        <br />
+        <br />
+        <input type="submit" value="Submit"/>
+      </form>
+    </div>
+  )
+}
+
+export default TwootForm
